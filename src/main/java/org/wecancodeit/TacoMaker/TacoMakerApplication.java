839424e15cf0eb4c310f3b2fb8fe0bc197279e1c
RickieCanCodeIT <rickie@WeCanCodeIT.org>
2021-05-27 08:53:36 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/TacoMaker/TacoMakerApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.TacoMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoMakerApplication.class, args);
+	}
+
+}
